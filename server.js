1db117458474b87399418535df66f6cd10c7d723
Johnny <hello@callbackapp.co.uk>
2026-07-27 11:14:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use('/api/settings', require('./routes/settings'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/photos', require('./routes/photos'));
 app.use('/api/events', require('./routes/events'));
+app.use('/api/leads', require('./routes/leads'));
 
 // Trial / payment check middleware
 async function trialCheck(req, res, next) {
